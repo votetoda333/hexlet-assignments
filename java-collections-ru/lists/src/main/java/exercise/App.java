51c518692c23daa4efb1dc05e4e49900a18f82c8
@hexlet/cli <support@hexlet.io>
2024-02-07 10:41:45 +0500

    submit java-collections-ru/lists

---
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -0,0 +1,16 @@
+package exercise;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+// BEGIN
+public  class App {
+
+    public static boolean scrabble() {
+
+
+        return true;
+    }
+}
+//END
