76b06043c9bf6fa5f11e7360dad2e96ae97aa3e4
@hexlet/cli <support@hexlet.io>
2024-02-07 12:57:26 +0500

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -7,8 +7,19 @@ import java.util.List;
 // BEGIN
 public  class App {
 
-    public static boolean scrabble() {
-
+    public static boolean scrabble(String letters, String word) {
+        if (letters == null || word == null) {
+            return false;
+        }
+        letters = letters.replaceAll("\\s+", "").toLowerCase();
+        word = word.replaceAll("\\s+", "").toLowerCase();
+        for (char c : word.toCharArray()) {
+            int index = letters.indexOf(c);
+            if (index == -1) {
+                return false;
+            }
+            letters = letters.substring(0, index) + letters.substring(index + 1);
+        }
 
         return true;
     }
